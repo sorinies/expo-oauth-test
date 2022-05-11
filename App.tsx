237eac4663e022d5b46d78d77673dd105da0de0d
sorinies <7258136+sorinies@users.noreply.github.com>
2022-05-11 18:16:38 +0900

    init

--- App.tsx
+++ App.tsx
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import * as WebBrowser from 'expo-web-browser';
+import * as Google from 'expo-auth-session/providers/google';
+import { Button } from 'react-native';
+
+WebBrowser.maybeCompleteAuthSession();
 
 export default function App() {
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    expoClientId: '206745855456-jf05aidm5hj0vjtdb0vugoa6jr68dodj.apps.googleusercontent.com',
+    iosClientId: '206745855456-8c1dd59ij1lj16l4ornnn4kfqh4v2272.apps.googleusercontent.com',
+    androidClientId: '206745855456-9emqvakbk18h8euteq19u2250bfqc1oq.apps.googleusercontent.com',
+    webClientId: '206745855456-5cng047njka26o3t5f0m4jb9c0olnint.apps.googleusercontent.com',
+  });
+
+  React.useEffect(() => {
+    if (response?.type === 'success') {
+      const { authentication } = response;
+      }
+  }, [response]);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Button
+      disabled={!request}
+      title="Login"
+      onPress={() => {
+        promptAsync();
+        }}
+    />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
